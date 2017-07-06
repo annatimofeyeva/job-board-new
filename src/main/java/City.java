14349c983a2492e81676f7986afc50d126ba5510
Anna Timofeeva and Esvethlynna Pangelinan <anna+esvethlynna@>
2017-07-06 10:43:22 -0700

    add City object and tests

--- src/main/java/City.java
+++ src/main/java/City.java
@@ -1,1 +1,49 @@
+import java.util.List;
+import java.util.ArrayList;
 
+public class City {
+  private String mName;
+  private static List<City> instances = new ArrayList<City>();
+  private int mId;
+  //This is an ArrayList that will eventually hold JobOpening objects belong to each instance of City.
+  private List<JobOpening> mJobOpenings;
+
+  public City(String name) {
+    mName = name;
+    instances.add(this);
+    mId = instances.size();
+    //Initialize an empty List<JobOpening> in the City constructor, since every single City must contain an attribute to hold corresponding JobOpenings.
+    mJobOpenings = new ArrayList<JobOpening>();
+  }
+
+  public String getName() {
+    return mName;
+  }
+
+  public static List<City> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static City find(int id) {
+    return instances.get(id - 1);
+  }
+
+  //Returns a City's list of JobOpenings.
+  public List<JobOpening> getJobs() {
+    return mJobOpenings;
+  }
+
+//This method will be called upon an individual City object, and a JobOpening object will be passed in as an argument. The method will then add that JobOpening to the City's mJobOpenings property. Once this method runs, the two objects should be successfully associated
+  public void addJobOpening(JobOpening jobOpening) {
+    mJobOpenings.add(jobOpening);
+  }
+
+}
---
+++ src/test/java/CityTest.java
@@ -0,0 +1,61 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CityTest {
+
+  @Test
+  public void city_instantiatesCorrectly_true() {
+    City testCity = new City("Seattle");
+    assertEquals(true, testCity instanceof City);
+  }
+
+  @Test
+  public void getName_cityInstantiatesWithName_Seattle() {
+    City testCity = new City("Seattle");
+    assertEquals("Seattle", testCity.getName());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfCity_true() {
+    City firstCity = new City("Seattle");
+    City secondCity = new City("Portland");
+    assertEquals(true, City.all().contains(firstCity));
+    assertEquals(true, City.all().contains(secondCity));
+  }
+
+  @Test
+  public void clear_emptiesAllCitiesFromList_0() {
+    City testCity = new City("Seattle");
+    City.clear();
+    assertEquals(City.all().size(), 0);
+  }
+
+  @Test
+  public void getId_citiesInstantiateWithAnId_1() {
+    City testCity = new City("Seattle");
+    assertEquals(1, testCity.getId());
+  }
+
+  @Test
+  public void find_returnsCityWithSameId_secondCity() {
+    City.clear();
+    City firstCity = new City("Seattle");
+    City secondCity = new City("Portland");
+    assertEquals(City.find(secondCity.getId()), secondCity);
+  }
+
+  @Test
+  public void getJobs_initiallyReturnsEmptyList_ArrayList() {
+    City.clear();
+    City testCity = new City("Seattle");
+    assertEquals(0, testCity.getJobs().size());
+  }
+
+  @Test
+  public void addJobOpening_addsJobOpeningToList_true() {
+    City testCity = new City("Seattle");
+    JobOpening testJobOpening = new JobOpening("Software Developer", "Write Java programs", "Anna and Lynn");
+    testCity.addJobOpening(testJobOpening);
+    assertTrue(testCity.getJobs().contains(testJobOpening));
+  }
+}
